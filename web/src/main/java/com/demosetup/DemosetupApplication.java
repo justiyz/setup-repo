c92cfd2f0b8290b83a098afd7c7d17a9677c5990
justiyz <inyange@yahoo.com>
2021-02-02 17:21:49 +0100

    first commit

---
+++ web/src/main/java/com/demosetup/DemosetupApplication.java
@@ -0,0 +1,13 @@
+package com.demosetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosetupApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/demosetup/DemosetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demosetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
